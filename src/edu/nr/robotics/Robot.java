a7f42603d17a2b17f193d8be420e6c47aecd5797
gbear605 <garrisontaylor@gmail.com>
2016-01-23 11:30:17 -0500

    Change Robot comment to remove reference to IterativeRobot

--- src/edu/nr/robotics/Robot.java
+++ src/edu/nr/robotics/Robot.java
@@ -26,10 +26,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
+ * The VM is configured to automatically run this class. 
+ * If you change the name of this class or the package after creating 
+ * this project, you must also update the manifest file in the resource
  * directory.
  */
 public class Robot extends RobotBase {
