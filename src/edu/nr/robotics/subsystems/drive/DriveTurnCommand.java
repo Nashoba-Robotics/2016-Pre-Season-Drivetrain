481d736b6d33f8ae4941c3e844381b7259f2a3af
gbear605 <garrisontaylor@gmail.com>
2016-01-28 19:25:42 -0500

    Add drive turn command

---
+++ src/edu/nr/robotics/subsystems/drive/DriveTurnCommand.java
@@ -0,0 +1,42 @@
+package edu.nr.robotics.subsystems.drive;
+
+import edu.nr.lib.AngleGyroCorrection;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveTurnCommand extends Command {
+
+	private AngleGyroCorrection gyro;
+	double angle;
+	
+    public DriveTurnCommand(double angle) {
+    	this.angle = angle;
+        requires(Drive.getInstance());
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	gyro = new AngleGyroCorrection(angle);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Drive.getInstance().arcadeDrive(0,gyro.getTurnValue());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return gyro.getAngleErrorDegrees() > 0;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
