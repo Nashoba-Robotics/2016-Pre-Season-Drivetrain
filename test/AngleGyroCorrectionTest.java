a99afda5863366efdceb323808ae7835ba0bfa46
gbear605 <garrisontaylor@gmail.com>
2016-02-13 13:29:23 -0500

    Add AngleGyroCorrection

---
+++ test/AngleGyroCorrectionTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.nr.lib.AngleGyroCorrection;
+import edu.nr.lib.navx.TestNavX;
+
+public class AngleGyroCorrectionTest {
+
+	AngleGyroCorrection correction = new AngleGyroCorrection(new TestNavX());
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
